e402c70eb094e8da400e33fece22fe43f69554c9
MRmlik12 <mrmlik12sup@gmail.com>
2021-06-30 16:39:00 +0200

    Add browser history

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ReactElement} from 'react';
+import {
+  Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import Home from './Views/Home';
 
-function App() {
+const history = createBrowserHistory();
+
+export default function App(): ReactElement {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <div className="App">
+        <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/Views/Home.tsx
@@ -0,0 +1,9 @@
+import React, {ReactElement} from 'react';
+
+export default function Home(): ReactElement {
+    return (
+      <div className="Home">
+          <h1>Hello, React</h1>
+      </div>
+    );
+}
