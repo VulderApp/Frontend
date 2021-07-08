66d61d290f38cca35e012e5996040224b6a2445d
MRmlik12 <mrmlik12sup@gmail.com>
2021-07-08 20:31:36 +0200

    Add eslint new rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,15 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
+    "settings": {
+        "react": {
+            "createClass": "createReactClass",
+            "pragma": "React",
+            "fragment": "Fragment",
+            "version": "detect",
+            "flowVersion": "0.53"
+        },
+    },
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
