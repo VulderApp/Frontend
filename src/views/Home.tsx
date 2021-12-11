e33359e80d918cd5a931f75cb328394826753ee8
MRmlik12 <mrmlik12sup@gmail.com>
2021-12-11 18:50:43 +0100

    Fix /timetable data loading

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,8 +2,14 @@ import { Container } from "@mui/material";
 import React, { ReactElement } from "react";
 import HomeLogo from "../components/home/HomeLogo";
 import HomeSearch from "../components/home/HomeSearch";
+import { useRecoilState } from "recoil";
+import { isTimetableView } from "../states";
 
 const Home = (): ReactElement => {
+  const [, setTimetableView] = useRecoilState(isTimetableView);
+
+  setTimetableView(false);
+
   return (
     <Container
       sx={{
