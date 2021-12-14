dcf7920a41381875b879b6515c7d7f3f49c9feac
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2021-12-14 20:38:13 +0100

    Add primary and secondary colors (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,34 @@ import React, { ReactElement } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./views/Home";
 import Appbar from "./components/root/Appbar";
-import { Toolbar } from "@mui/material";
+import { createTheme, ThemeProvider, Toolbar } from "@mui/material";
 import Timetable from "./views/Timetable";
 import { RecoilRoot } from "recoil";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ffc107",
+    },
+    secondary: {
+      main: "#ff9100",
+    },
+  },
+});
 
 const App = (): ReactElement => {
   return (
     <RecoilRoot>
-      <BrowserRouter>
-        <Appbar />
-        <Toolbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/timetable" element={<Timetable />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Appbar />
+          <Toolbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/timetable" element={<Timetable />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </RecoilRoot>
   );
 };
