87d842efea5dfded1d1dcca3be4a850964409b5b
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2022-02-24 11:05:26 +0100

    Add webpack API_URL env (#66)

--- craco.config.js
+++ craco.config.js
@@ -6,6 +6,7 @@ module.exports = {
     plugins: [
       new webpack.DefinePlugin({
         VERSION: JSON.stringify(require("./package.json").version),
+        "process.env.API_URL": JSON.stringify(process.env.API_URL),
       }),
     ],
   },
